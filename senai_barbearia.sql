c92b57062ca7e4d63c1d35f2536537ac80ce2fe4
Caamano-01 <isabelecaamano87@gmail.com>
2025-12-11 17:18:23 -0300

    banco de dados com atulizações

--- senai_barbearia.sql
+++ senai_barbearia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11/12/2025 às 18:56
+-- Tempo de geração: 11/12/2025 às 21:17
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
