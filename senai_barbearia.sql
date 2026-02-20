3f787fa8a4a7b43c6d27392df7e66d5f4b5002b1
Caamano-01 <isabelecaamano87@gmail.com>
2026-02-20 16:22:53 -0300

    serviços atualizados e com style melhor

--- senai_barbearia.sql
+++ senai_barbearia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19-Fev-2026 às 20:54
+-- Tempo de geração: 20-Fev-2026 às 19:12
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -38,13 +38,6 @@ CREATE TABLE `agendamentos` (
   `criado_em` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `agendamentos`
---
-
-INSERT INTO `agendamentos` (`id`, `usuario_id`, `servico_id`, `profissional_id`, `data`, `hora`, `status`, `criado_em`) VALUES
-(1, 2, 2, 3, '2026-02-27', '14:00:00', 'confirmado', '2026-02-19 19:49:44');
-
 -- --------------------------------------------------------
 
 --
@@ -59,6 +52,13 @@ CREATE TABLE `bloqueios_horarios` (
   `hora_fim` time NOT NULL,
   `motivo` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `bloqueios_horarios`
+--
+
+INSERT INTO `bloqueios_horarios` (`id`, `profissional_id`, `data`, `hora_inicio`, `hora_fim`, `motivo`) VALUES
+(1, 4, '2026-02-28', '09:00:00', '12:00:00', 'Consulta ao médico');
 
 -- --------------------------------------------------------
 
@@ -85,17 +85,18 @@ CREATE TABLE `profissionais` (
   `nome` varchar(100) NOT NULL,
   `contato` varchar(50) DEFAULT NULL,
   `foto_url` varchar(255) DEFAULT NULL,
-  `ativo` tinyint(1) DEFAULT 1
+  `ativo` tinyint(1) DEFAULT 1,
+  `usuario_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `profissionais`
 --
 
-INSERT INTO `profissionais` (`id`, `nome`, `contato`, `foto_url`, `ativo`) VALUES
-(1, 'Márcio', '(51) 9876-54321', 'https://res.cloudinary.com/dqsodebo9/image/upload/v1771444200/ddznobwbtzlooy50igsh.jpg', 1),
-(2, 'Daniel', '(51)1234-56789', 'https://res.cloudinary.com/dqsodebo9/image/upload/v1771521033/gvy9eucggphbw3xioijk.jpg', 1),
-(3, 'André', '(51) 9976-55321', 'https://res.cloudinary.com/dqsodebo9/image/upload/v1771521060/zrs1sruddc8bcqbcezjd.jpg', 1);
+INSERT INTO `profissionais` (`id`, `nome`, `contato`, `foto_url`, `ativo`, `usuario_id`) VALUES
+(4, 'Márcio', '(51) 9976-55321', 'https://res.cloudinary.com/dqsodebo9/image/upload/v1771607517/gn579ohzekzgeb6xrn9h.jpg', 1, 5),
+(5, 'Daniel', '(51)1234-56789', 'https://res.cloudinary.com/dqsodebo9/image/upload/v1771607804/ydq3pnvvbbnuuzuqpofa.jpg', 1, 6),
+(6, 'André', '(51) 9876-54321', 'https://res.cloudinary.com/dqsodebo9/image/upload/v1771608555/xruo9n5jnoydqnkpco28.jpg', 1, 8);
 
 -- --------------------------------------------------------
 
@@ -114,12 +115,15 @@ CREATE TABLE `profissional_servico` (
 --
 
 INSERT INTO `profissional_servico` (`id`, `profissional_id`, `servico_id`) VALUES
-(4, 1, 1),
-(5, 1, 2),
-(6, 2, 1),
-(7, 2, 3),
-(8, 3, 2),
-(9, 3, 3);
+(10, 4, 1),
+(11, 4, 2),
+(12, 4, 4),
+(13, 5, 1),
+(14, 5, 3),
+(15, 6, 1),
+(16, 6, 2),
+(17, 6, 3),
+(18, 6, 4);
 
 -- --------------------------------------------------------
 
@@ -168,7 +172,10 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha_hash`, `telefone`, `tipo`, `data_cadastro`) VALUES
 (1, 'Administrador', 'admin@teste.com', '$2y$10$P9HGK8ZYr.MAwYAQV9cGDeQkJwgO8m6aMWbesL0QDOjmhjNf8bNze', '51888888888', 'admin', '2026-02-13 18:15:49'),
-(2, 'Usuário Teste', 'usuario@teste.com', '$2y$10$7qYtZ5LlKZDMcKq6V8pqqOSlyiZ8u.NaKb3BEqfJGH4O/o0dsEq8u', '51999999999', 'cliente', '2026-02-13 18:15:09');
+(2, 'Usuário Teste', 'usuario@teste.com', '$2y$10$7qYtZ5LlKZDMcKq6V8pqqOSlyiZ8u.NaKb3BEqfJGH4O/o0dsEq8u', '51999999999', 'cliente', '2026-02-13 18:15:09'),
+(5, 'Márcio', 'marcio-barber@teste.com', '$2y$10$h.dMbB/poCPmihLHJ5pY2.JkDLmWJAeuR85YqJKYFXknu/HVIWAJO', '(51) 9976-55321', 'admin', '2026-02-20 17:11:57'),
+(6, 'Daniel', 'daniel-barber@teste.com', '$2y$10$Lh6zok1dswfkYMp4wgmxyu1MnXKCeaRaNut4b0NbOHe7k/QThPIfa', '(51)1234-56789', 'admin', '2026-02-20 17:16:44'),
+(8, 'André', 'andre-barber@teste.com', '$2y$10$zOy.kRIHT7r6SLcLtqS.C.bVYBN9DN7M4T4988TIbMSSvGjVXn65q', '(51) 9876-54321', 'admin', '2026-02-20 17:29:15');
 
 --
 -- Índices para tabelas despejadas
@@ -201,7 +208,8 @@ ALTER TABLE `faturamento`
 -- Índices para tabela `profissionais`
 --
 ALTER TABLE `profissionais`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_usuario_profissional` (`usuario_id`);
 
 --
 -- Índices para tabela `profissional_servico`
@@ -238,7 +246,7 @@ ALTER TABLE `agendamentos`
 -- AUTO_INCREMENT de tabela `bloqueios_horarios`
 --
 ALTER TABLE `bloqueios_horarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `faturamento`
@@ -250,13 +258,13 @@ ALTER TABLE `faturamento`
 -- AUTO_INCREMENT de tabela `profissionais`
 --
 ALTER TABLE `profissionais`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `profissional_servico`
 --
 ALTER TABLE `profissional_servico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de tabela `servicos`
@@ -268,7 +276,7 @@ ALTER TABLE `servicos`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restrições para despejos de tabelas
@@ -293,6 +301,12 @@ ALTER TABLE `bloqueios_horarios`
 --
 ALTER TABLE `faturamento`
   ADD CONSTRAINT `faturamento_ibfk_1` FOREIGN KEY (`agendamento_id`) REFERENCES `agendamentos` (`id`);
+
+--
+-- Limitadores para a tabela `profissionais`
+--
+ALTER TABLE `profissionais`
+  ADD CONSTRAINT `fk_usuario_profissional` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL;
 
 --
 -- Limitadores para a tabela `profissional_servico`
